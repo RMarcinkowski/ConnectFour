bf7e0c25f2d73f49f74e765bd6d4712059c967a3
RMarcinkowski <32728206+RMarcinkowski@users.noreply.github.com>
2018-09-19 12:56:35 +0200

    Update view.py

--- view.py
+++ view.py
@@ -1,2 +1,11 @@
-def set_disc_position():
-    return 1
+def set_disc_position(name):
+    position_set = False
+    while not position_set:
+        position = input(name + ", wähle eine Spalte von 0-6: ")
+        try:
+            position = int(position)
+            if position < 0 or position > 6:
+                raise IndexError()
+            return position
+        except:
+            print("Es muss eine Zahl von 0-6 sein!")
